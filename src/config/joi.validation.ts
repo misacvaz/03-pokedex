be842bcd3794bbd35c2b28d5d412e4513bd884f6
misacvaz <misacvaz@gmail.com>
2023-09-05 12:54:43 -0100

    pokedex

---
+++ src/config/joi.validation.ts
@@ -0,0 +1,8 @@
+import * as Joi from "joi";
+
+
+export const JoiValidationSchema = Joi.object({
+    MONGODB: Joi.required(),
+    PORT: Joi.number().default(3005),
+    DEFOULT_LIMIT: Joi.number().default(6)
+})
---
+++ src/seed/interfaces/poke-response.interface.ts
@@ -0,0 +1,14 @@
+
+export interface PokeResponse {
+    count:    number;
+    next:     string;
+    previous: null;
+    results:  Result[];
+}
+
+export interface Result {
+    name: string;
+    url:  string;
+}
+
+		
